d9fb6ad61ea9703ac9e02cd48b1904e40b880237
Тимофей Городилов <timotheos94@protonmail.com>
2022-08-09 18:57:59 +0400

    first steps

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # homework_bot
-python telegram bot
+python telegram bot - находит домашку пользователя на Я.Практикуме и сообщает когда она проверена
--- homework.py
+++ homework.py
@@ -1,11 +1,17 @@
-...
+import logging
+import os
+
+import requests
+from dotenv import load_dotenv
+from telegram import Bot, ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -15,7 +21,7 @@ HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
+    'rejected': 'Работа проверена: у ревьюера есть замечания.',
 }
 
 
@@ -26,25 +32,19 @@ def send_message(bot, message):
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
     ...
 
 
 def check_response(response):
-
     ...
 
 
 def parse_status(homework):
     homework_name = ...
     homework_status = ...
-
     ...
-
     verdict = ...
-
     ...
-
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
