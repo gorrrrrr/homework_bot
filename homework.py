d505452e34f0e1d979bfa1b40bc0bec4eedde8d4
Тимофей Городилов <timotheos94@protonmail.com>
2022-08-26 20:45:57 +0400

    restore progress

--- homework.py
+++ homework.py
@@ -1,75 +1,86 @@
 import logging
-import os
 import time
-
-import requests
 from dotenv import load_dotenv
 from telegram import Bot
-from telegram.ext import CommandHandler, Filters, MessageHandler
+import requests
+import settings
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-
-RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
-logging.basicConfig(
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    level=logging.INFO)
-
-HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.',
-}
-
-
 def send_message(bot, message):
+    """Отправляет сообщение в Telegram чат, определяемый TELEGRAM_CHAT_ID.
+    Принимает на вход два параметра: экземпляр класса Bot и сообщение.
+    """
     ...
 
 
 def get_api_answer(current_timestamp):
-    timestamp = current_timestamp or int(time.time())
+    """Делает запрос к эндпоинту API-сервиса.
+    В качестве параметра функция получает временную метку. В случае успешного
+    запроса возвращает ответ API, преобразовав к типам данных Python.
+    """
+    timestamp = current_timestamp - settings.RETRY_TIME
     params = {'from_date': timestamp}
-    ...
+    try:
+        homeworks = requests.get(settings.ENDPOINT,
+                                 headers=settings.HEADERS,
+                                 params=params)
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к API: {error}')
+    return homeworks.json()
 
 
 def check_response(response):
-    ...
+    """Проверяет ответ API на корректность.
+    В качестве параметра функция получает ответ API, приведенный к типам
+    данных Python. Если ответ API соответствует ожиданиям, то функция должна
+    вернуть список домашних работ (он может быть и пустым), доступный в ответе
+    API по ключу 'homeworks'.
+    """
+    return response['homeworks']
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
-    ...
-    verdict = ...
-    ...
+    """Извлекает из информации о конкретной домашней работе статус этой работы.
+    В качестве параметра функция получает только один элемент из списка
+    домашних работ. В случае успеха, функция возвращает подготовленную для
+    отправки в Telegram строку, содержащую один из вердиктов словаря 
+    HOMEWORK_STATUSES.
+    """
+    homework_name = homework['homework_name']
+    homework_status = homework['status']
+    verdict = settings.HOMEWORK_STATUSES[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    ...
+    """Проверяет доступность необходимых переменных окружения."""
+    return bool(settings.PRACTICUM_TOKEN
+                and settings.TELEGRAM_TOKEN
+                and settings.TELEGRAM_CHAT_ID)
 
 
 def main():
     """Основная логика работы бота."""
-    bot = Bot(token=TELEGRAM_TOKEN)
+    bot = Bot(token=settings.TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
+
+    ...
+
     while True:
         try:
-            response = 
-            current_timestamp = 
-            time.sleep(RETRY_TIME)
+            response = ...
+
+            ...
+
+            current_timestamp = int(time.time())
+            time.sleep(settings.RETRY_TIME)
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             ...
-            time.sleep(RETRY_TIME)
+            time.sleep(settings.RETRY_TIME)
         else:
             ...
 
