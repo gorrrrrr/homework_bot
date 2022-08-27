2f3bea9d6621853ced37b3ad3348f652b3f3c523
Тимофей Городилов <timotheos94@protonmail.com>
2022-08-27 16:25:19 +0400

    before tests

--- homework.py
+++ homework.py
@@ -1,21 +1,21 @@
 import logging
 import time
-from dotenv import load_dotenv
 from telegram import Bot
 import requests
 import settings
 from exceptions import MissingCostantException, ExpectedDictException
 
-load_dotenv()
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def send_message(bot, message):
     """Отправляет сообщение в Telegram чат, определяемый TELEGRAM_CHAT_ID."""
     try:
         bot.send_message(settings.TELEGRAM_CHAT_ID, message)
-        logging.INFO(f'Отправлено сообщение: {message}')
+        logging.info(f'Отправлено сообщение: {message}')
     except Exception as error:
-        logging.ERROR(f'Ошибка при отправке сообщения: {error}')
+        logging.error(f'Ошибка при отправке сообщения: {error}')
 
 
 def get_api_answer(current_timestamp):
@@ -27,15 +27,14 @@ def get_api_answer(current_timestamp):
                             headers=settings.HEADERS,
                             params=params).json()
     except Exception as error:
-        logging.ERROR(f'Ошибка при запросе к API: {error}')
-    return None
+        logging.error(f'Ошибка при запросе к API: {error}')
 
 
 def check_response(response):
     """Проверяет ответ API на корректность."""
     if type(response) is not dict:
         msg = 'Ответ API не является словарём!'
-        logging.ERROR(msg)
+        logging.error(msg)
         raise ExpectedDictException(msg)
 
     return response['homeworks']
@@ -44,14 +43,14 @@ def check_response(response):
 def parse_status(homework):
     """Достаёт из информации о конкретной ДЗ её статус. Возвращает вердикт."""
     if not bool(homework['homework_name'] and homework['status']):
-        logging.ERROR('В ответе API не хватает требуемых ключей')
+        logging.error('В ответе API не хватает требуемых ключей')
     homework_name = homework['homework_name']
     homework_status = homework['status']
     try:
         verdict = settings.HOMEWORK_STATUSES[homework_status]
         return f'Изменился статус проверки работы "{homework_name}". {verdict}'
     except Exception as error:
-        logging.ERROR(f'Ошибка присвоения статуса ДЗ: {error}')
+        logging.error(f'Ошибка присвоения статуса ДЗ: {error}')
         return f'Изменился статус проверки работы "{homework_name}".'
 
 
@@ -64,9 +63,6 @@ def check_tokens():
 
 def main():
     """Основная логика работы бота."""
-    print(settings.TELEGRAM_TOKEN)
-    print(settings.TELEGRAM_CHAT_ID)
-    print(settings.PRACTICUM_TOKEN)
     bot = Bot(token=settings.TELEGRAM_TOKEN)
 
     if check_tokens() is False:
@@ -74,28 +70,27 @@ def main():
         try:
             send_message(bot, msg)
         except Exception as error:
-            logging.CRITICAL(f'Не выходит сообщить об ошибке в тг: {error}')
+            logging.critical(f'Не выходит сообщить об ошибке в тг: {error}')
         raise MissingCostantException(msg)
 
-    current_timestamp = int(time.time())
     while True:
         try:
+            current_timestamp = int(time.time())
             response = get_api_answer(current_timestamp)
             homeworks = check_response(response)
             if not homeworks:
-                logging.DEBUG('Нет новых вердиктов по запросу.')
+                logging.debug('Нет новых вердиктов по запросу.')
                 time.sleep(settings.RETRY_TIME)
             else:
                 for hw in homeworks:
                     msg = parse_status(hw)
                     send_message(bot, msg)
 
-            current_timestamp = int(time.time())
             time.sleep(settings.RETRY_TIME)
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            logging.ERROR(message)
+            logging.error(message)
             time.sleep(settings.RETRY_TIME)
 
 
