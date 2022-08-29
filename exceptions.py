eec186fa43fcd444210f56051d991a4d4b06bf71
Тимофей Городилов <timotheos94@protonmail.com>
2022-08-30 00:02:08 +0400

    after 1 rwiew

--- exceptions.py
+++ exceptions.py
@@ -8,3 +8,14 @@ class NotExpectedHwStatusException(Exception):
     """API answer contains unknown Hw status."""
 
     pass
+
+
+class NotOkResponseExeption(Exception):
+    """Request got not OK Status."""
+
+    pass
+
+
+""" Не понял мысль: Для первых двух удобно завести общий базовый
+класс и делать его перехват в main().
+"""
--- homework.py
+++ homework.py
@@ -1,106 +1,130 @@
 import logging
+import sys
 import time
-from telegram import Bot
+from http import HTTPStatus
+
 import requests
-from settings import (PRACTICUM_TOKEN, TELEGRAM_CHAT_ID, TELEGRAM_TOKEN,
-                      RETRY_TIME, ENDPOINT, HEADERS, HOMEWORK_STATUSES)
-import exceptions
+from telegram import Bot
+from telegram.error import TelegramError
 
-
-logging.basicConfig(level=logging.DEBUG)
+import exceptions as e
+from settings import (ENDPOINT, HEADERS, HOMEWORK_VERDICTS, PRACTICUM_TOKEN,
+                      RETRY_TIME, TELEGRAM_CHAT_ID, TELEGRAM_TOKEN,
+                      constant_tuple)
 
 
 def send_message(bot, message):
     """Отправляет сообщение в Telegram чат, определяемый TELEGRAM_CHAT_ID."""
+    logging.debug(f'Собираюсь отправить в телеграм сообщение: {message}.')
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
+    except TelegramError as error:
+        raise TelegramError(f'Ошибка при отправке телеграм сообщения: {error}')
+    else:
         logging.info(f'Отправлено сообщение: {message}')
-    except Exception as error:
-        logging.error(f'Ошибка при отправке сообщения: {error}')
 
 
 def get_api_answer(current_timestamp):
     """Запрашивает эндпоинт API. При успехе возвращает ответ API type dict."""
+    logging.debug('Запрос к API.')
     timestamp = current_timestamp
-    params = {'from_date': timestamp}
-    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-    if response.status_code != 200:
-        logging.error('Endpoint Error.')
-        raise Exception
+    request_params = {'url': ENDPOINT, 'headers': HEADERS,
+                      'params': {'from_date': timestamp}, 'timeout': 10}
+    "Может проще это в response сразу писать? Создаётся лишняя сущность."
+    try:
+        response = requests.get(**request_params)
+    except TimeoutError as e:
+        raise TimeoutError(f'Ошибка {e}')
+    if response.status_code != HTTPStatus.OK:  # а в чём смысл заменять?
+        raise e.NotOkResponseExeption('ответ не ОК.')
     return response.json()
 
 
 def check_response(response):
     """Проверяет ответ API на корректность."""
-    if type(response) is not dict:
-        msg = 'Ответ API не является словарём!'
-        logging.error(msg)
-        raise TypeError(msg)
-    hw = response['homeworks']
-    if type(hw) is not list:
-        msg = 'homeworks не является списком!'
-        logging.error(msg)
-        raise TypeError(msg)
-    return hw
+    logging.debug('response получен, проверяем.')
+    if not isinstance(response, dict):
+        raise TypeError(f'Ответ API не словарь, а {type(response)}'
+                        f'следующего содержания: {response}')
+    if 'homeworks' and 'current_date' not in response:
+        keysList = list(response.keys())
+        raise KeyError('В ответе API нет нужных ключей.'
+                       f'Вот какие есть: {keysList}')
+    homeworks = response['homeworks']
+    if not isinstance(homeworks, list):
+        raise TypeError('В ответе от API под ключом "homeworks"'
+                        f'пришел не список, а {type(homeworks)}'
+                        f'следующего содержания: {homeworks}')
+    return homeworks
 
 
 def parse_status(homework):
     """Достаёт из информации о конкретной ДЗ её статус. Возвращает вердикт."""
-    if not bool(homework['homework_name'] and homework['status']):
-        logging.error('В ответе API не хватает требуемых ключей')
-    homework_name = homework['homework_name']
-    homework_status = homework['status']
-    if homework_status not in HOMEWORK_STATUSES.keys():
-        msg = 'В ответе API неизвестный статус ДЗ!'
-        logging.error(msg)
-        raise exceptions.NotExpectedHwStatusException(msg)
-    try:
-        verdict = HOMEWORK_STATUSES[homework_status]
-        return f'Изменился статус проверки работы "{homework_name}". {verdict}'
-    except Exception as error:
-        logging.error(f'Ошибка присвоения статуса ДЗ: {error}')
-        return f'Изменился статус проверки работы "{homework_name}".'
+    logging.debug('Начали парсить.')
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    if not bool(homework_name and homework_status):
+        keysList = list(homework.keys())
+        raise KeyError(f'В ДЗ нет нужных ключей. Вот какие есть: {keysList}')
+    if homework_status not in HOMEWORK_VERDICTS:
+        raise e.NotExpectedHwStatusException('В ответе API неизвестный статус'
+                                             f'ДЗ: {homework_status}')
+    verdict = HOMEWORK_VERDICTS[homework_status]
+    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
     """Проверяет доступность необходимых переменных окружения."""
-    return bool(PRACTICUM_TOKEN
-                and TELEGRAM_TOKEN
-                and TELEGRAM_CHAT_ID)
+    return all((PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID))
 
 
 def main():
     """Основная логика работы бота."""
-    bot = Bot(token=TELEGRAM_TOKEN)
-
     if check_tokens() is False:
-        msg = 'Не хватает необходимых констант!'
-        try:
-            send_message(bot, msg)
-        except Exception as error:
-            logging.critical(f'Не выходит сообщить об ошибке в тг: {error}')
-        raise exceptions.MissingCostantException(msg)
-    current_timestamp = int(time.time())
+        msg = 'В работе Не хватает необходимых констант:'
+        for i in constant_tuple:
+            if not i:
+                msg = msg + i
+        logging.critical(msg)
+        sys.exit(msg)
 
+    bot = Bot(token=TELEGRAM_TOKEN)
+    current_timestamp = int(time.time())
+    prev_report = ''
+    """Показалось проще в переменную prev_report передавать str"""
     while True:
         try:
             response = get_api_answer(current_timestamp)
             homeworks = check_response(response)
             if not homeworks:
                 logging.debug('Нет новых вердиктов по запросу.')
-                time.sleep(RETRY_TIME)
             else:
-                for hw in homeworks:
-                    msg = parse_status(hw)
+                msg = parse_status(homeworks[0])
+                if prev_report != msg:
+                    prev_report = msg
                     send_message(bot, msg)
 
-            time.sleep(RETRY_TIME)
-
+        except TimeoutError as error:
+            logging.error(error)
+        except e.NotOkResponse as error:
+            logging.error(error)
+        except TypeError as error:
+            logging.error(error)
+        except TypeError as error:
+            logging.error(error)
+        except KeyError as error:
+            logging.error(error)
+        except e.NotExpectedHwStatusException as error:
+            logging.error(error)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            logging.error(message)
+            logging.error(message, exc_info=True)
+            send_message(bot, message)
+        finally:
             time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s, %(levelname)s, %(message)s')
     main()
